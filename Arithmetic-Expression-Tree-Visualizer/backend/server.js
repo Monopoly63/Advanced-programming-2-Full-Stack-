8019a6f79c24587d29e08bd2e72c4ef928db0899
Abdulmoin Hablas <144657407+Monopoly63@users.noreply.github.com>
2026-05-03 21:40:48 +0300

    Delete Arithmetic-Expression-Tree-Visualizer directory

--- Arithmetic-Expression-Tree-Visualizer/backend/server.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const app = require('./src/app');
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`✅ Server running on http://localhost:${PORT}`);
-});
